b7bc3f1fef0dfc651ccd6863fc47abfdf12f15d3
Unknown <jdavid_nel@yahoo.fr>
2018-07-07 05:25:26 +0200

    Start of the apps

--- Epicture/MainPage.xaml.cs
+++ Epicture/MainPage.xaml.cs
@@ -26,5 +26,14 @@ namespace Epicture
         {
             this.InitializeComponent();
         }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Bonjour, Nassim Ben-Yaaghoub le noob sur LOLE");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
     }
 }
